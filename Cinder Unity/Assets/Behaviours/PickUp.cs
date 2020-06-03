642b608250dc40ecd87b1605f54351272c9c082d
draggmaste <sigurd.vatnehol@gmail.com>
2020-06-03 14:59:47 +0200

    More updates, soon ready proto type, fixed some level design for the prototype and added some lines in the pick up system to dissable Cinder from climbing and double jumping while holding a item

--- Cinder Unity/Assets/Behaviours/PickUp.cs
+++ Cinder Unity/Assets/Behaviours/PickUp.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     private Collider2D touchingItem;
+    private float originalClimbSpeed;
 
     public Transform world;
     public LayerMask whatIsPickup;
     public Transform checkPosition;
     public float checkRadius;
 
+    private void Start()
+    {
+        originalClimbSpeed = GetComponent<Climb>().climbSpeed;
+    }
+
     public void dropItem(HeldItem item)
     {
         item.transform.parent = world;
         Rigidbody2D rb = item.transform.GetComponent<Rigidbody2D>();
         rb.simulated = true;
+        GetComponent<Jump>().extraJumpValue = 1;
+        GetComponent<Climb>().climbSpeed = originalClimbSpeed;
     }
 
     public HeldItem pickupItem(Transform holdPos)
@@ -27,6 +35,8 @@ public class PickUp : MonoBehaviour
             touchingItem.transform.localPosition = holdPos.localPosition;
             Rigidbody2D rb = touchingItem.GetComponent<Rigidbody2D>();
             rb.simulated = false;
+            GetComponent<Jump>().extraJumpValue = 0;
+            GetComponent<Climb>().climbSpeed = 0;
             return touchingItem.GetComponent<HeldItem>();
         }
         return null;
