1eae6a03e173b936d0500875afa754aaeef8fdf2
MysticVoice <Fredrimv@hotmail.com>
2020-06-23 18:50:59 +0200

    merge completion

--- Cinder Unity/Assets/Environment/Climbable.cs
+++ Cinder Unity/Assets/Environment/Climbable.cs
@@ -37,11 +37,7 @@ public class Climbable : InteractableTrigger
         if (other.tag == "Player")
         {
             playerInRange = false;
-<<<<<<< HEAD
             text.SetActive(false);
-=======
-            Debug.Log("Player Left!");
->>>>>>> d63fd431a76aeece984e97f393b1d702741e1e53
         }
     }
 
